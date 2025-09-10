f4f0f24dc88f1c22225ae152780604146009f973
karlavdf <karlavdelgadof@gmail.com>
2025-09-10 16:32:44 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "McFly Education | Aviation Training & Pilot School";
+export const SITE_DESCRIPTION = "Welcome to McFly Education!";
 export const PHONE_NUMBER = "+1 (918) 770-1201";
-export const INSTAGRAM_URL = "https://www.instagram.com/truflight.academy/";
-export const FACEBOOK_URL = "https://www.facebook.com/truflightacademy";
+export const INSTAGRAM_URL = "";
+export const FACEBOOK_URL = "";
 export const ADDRESS_LINE_1 = "9007 FM 902";
 export const ADDRESS_LINE_2 = "Howe, TX 75459";
 export const AIRPORT = "Finney Field Airport (T47)";
